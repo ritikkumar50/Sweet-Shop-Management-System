18c4fb7862cfe5c3928fd9a6943e7adf8c3e4420
Ritik Kumar <ritikbnl2020@gmail.com>
2025-12-13 19:27:50 +0530

    Getting started

---
+++ Sweet Shop/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#ec4899', // Pink
+                secondary: '#2dd4bf', // Teal
+                accent: '#f59e0b', // Amber
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+}
