a170dcc5848b3a71059617aa7299a30ff5690051
Ritik Kumar <ritikbnl2020@gmail.com>
2025-12-13 23:48:46 +0530

    deployement

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    });
+}
+
+module.exports = app;
